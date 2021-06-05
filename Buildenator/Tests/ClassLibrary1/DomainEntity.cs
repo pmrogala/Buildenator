2ee60bc831df46e2cf1ee9e8083c8abcaec2cc22
Przemysław Rogala <progala@ecovadis.com>
2021-06-05 23:45:34 +0200

    A clever approach for unit testing the generator

---
+++ Buildenator/Tests/ClassLibrary1/DomainEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Buildenator.UnitTests.Source
+{
+    public class DomainEntity
+    {
+        public DomainEntity(int propertyIntGetter, string propertyStringGetter)
+        {
+            PropertyIntGetter = propertyIntGetter;
+            PropertyGetter = propertyStringGetter;
+        }
+
+        public int PropertyIntGetter { get; private set; }
+        public string PropertyGetter { get; private set; }
+    }
+}
