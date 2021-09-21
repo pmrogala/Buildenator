5c2fe54af9526661b253f2989be1586e63c083e7
Przemysław Rogala <progala@ecovadis.com>
2021-09-21 23:36:41 +0200

    ### Added
    - Handling generic types

--- Tests/Buildenator.IntegrationTests.SharedEntities/GrandchildEntity.cs
+++ Tests/Buildenator.IntegrationTests.SharedEntities/GrandchildEntity.cs
@@ -13,4 +13,19 @@ namespace Buildenator.IntegrationTests.SharedEntities
 
         public IInterfaceType InterfaceType { get; set; }
     }
+
+    public class GrandchildEntity<T, TK> : ChildEntity
+        where T : struct
+        where TK : notnull, EntityInDifferentNamespace, new()
+    {
+        public GrandchildEntity(int propertyIntGetter, string propertyStringGetter, EntityInDifferentNamespace entityInDifferentNamespace, List<string> protectedProperty, IEnumerable<int> privateField, TK secondGeneric) : base(propertyIntGetter, propertyStringGetter, entityInDifferentNamespace, protectedProperty, privateField)
+        {
+            SecondGeneric = secondGeneric;
+        }
+
+        protected override List<string> ProtectedProperty => base.ProtectedProperty;
+
+        public T GenericType { get; set; }
+        public TK SecondGeneric { get; }
+    }
 }
