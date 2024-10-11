ca81cecbf03b283a7ffd64c0e8ab6dd613e36b7c
progala@ecovadis.com <progala@ecovadis.com>
2024-10-11 22:19:37 +0200

    ## 6.1.2 & 8.1.2 - 2024-10-11
    ### Changed
    
    - Very weird issue only reproducible after creating and using nuget package. Filtering implicitly created constructors helped by some reason.
    - Versioning number to the generation string

---
+++ Tests/Buildenator.IntegrationTests.SharedEntities/EntityWithImplicitConstructor.cs
@@ -0,0 +1,8 @@
+﻿namespace Buildenator.IntegrationTests.SharedEntities;
+
+public class EntityWithImplicitConstructor
+{
+
+    public int PropertyIntGetter { get; } = 1;
+    public string PropertyGetter { get; } = "1";
+}
